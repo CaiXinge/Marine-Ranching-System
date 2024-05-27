054d87e071bc5879345d45587fdd8eb0ea873c08
caixinge <cxg1239482548@outlook.com>
2024-05-27 23:02:25 +0800

    underwater system upgrade

--- main.py
+++ main.py
@@ -1,10 +1,11 @@
-from flask import Flask, render_template, request, redirect, url_for, flash
+from flask import Flask, render_template, request, redirect, url_for
 
 import db_query
 import datetime
+from environment_rank import total_rank
 
 app = Flask(__name__)
-app.config['SECRET_KEY']='gsolvit'
+app.config['SECRET_KEY'] = 'gsolvit'
 
 
 @app.route('/')
@@ -20,7 +21,7 @@ def admin_login():
         admin_id = request.form.get('admin_id')
         password = request.form.get('password')
         sql = 'select* from admin where admin_id=%s and password=%s'
-        result = db_query.db_query(sql, [admin_id,password])
+        result = db_query.db_query(sql, [admin_id, password])
         print(result)
         if len(result) != 0:
             if result[0]['password'] == password:
@@ -29,7 +30,7 @@ def admin_login():
                 error = '账号或密码错误'
         else:
             error = '账号或密码错误'
-        return render_template('admin_login.html',error=error)
+        return render_template('admin_login.html', error=error)
 
 
 @app.route('/admin')
@@ -45,7 +46,7 @@ def user_login():
         user_id = request.form.get('user_id')
         password = request.form.get('password')
         sql = 'select* from user where user_id=%s and password=%s'
-        result = db_query.db_query(sql, [user_id,password])
+        result = db_query.db_query(sql, [user_id, password])
         print(result)
         if len(result) != 0:
             return redirect(url_for(('user')))
@@ -59,31 +60,50 @@ def user():
     sql = 'select* from 水质数据 where date=%s'
     result = db_query.db_query(sql, datetime.date.today())
     print(result)
-    if request.method=='GET':
+    if request.method == 'GET':
         return render_template('user_index.html', water_quality=result)
     else:
-        start_date_str=request.form.get('startDate')
-        end_date_str=request.form.get('endDate')
-        feature=request.form.get('feature')
-        print(start_date_str,end_date_str,feature)
+        start_date_str = request.form.get('startDate')
+        end_date_str = request.form.get('endDate')
+        feature = request.form.get('feature')
+        print(start_date_str, end_date_str, feature)
         start_date = datetime.datetime.strptime(start_date_str, '%Y-%m-%d').date()
         end_date = datetime.datetime.strptime(end_date_str, '%Y-%m-%d').date()
         sql = 'select * from 水质数据 where date BETWEEN %s AND %s'
         result1 = db_query.db_query(sql, [start_date, end_date])
         for data in result1:
             print(data)
-        return render_template('user_index.html', water_quality=result, history_data=result1,feature=feature)
+        return render_template('user_index.html', water_quality=result, history_data=result1, feature=feature)
 
 
-
-@app.route('/main_information')
-def main_information():
-    return render_template('main_information.html')
-
-
-@app.route('/underwater_system')
+@app.route('/underwater_system', methods=['GET', 'POST'])
 def underwater_system():
-    return render_template('underwater_system.html')
+    sql = 'select* from 水质数据 where date=%s'
+    result = db_query.db_query(sql, datetime.date.today())
+    print(result)
+    rank = total_rank(result[0])
+    sql1 = 'select* from fish_num'
+    nums = db_query.db_query(sql1)
+    print(nums)
+    sql2 = 'SELECT Species,COUNT(*) AS SpeciesNum FROM fish GROUP BY Species'
+    species_num = db_query.db_query(sql2)
+    print(species_num)
+    species = [item['Species'] for item in species_num]
+    if request.method == 'GET':
+        return render_template('underwater_system.html', rank=rank, nums=nums, specie_num=len(species_num),
+                               species=species,species_num=species_num)
+    else:
+        specie = request.form.get('specie')
+        feature = request.form.get('feature')
+        print(specie)
+        print(feature)
+        sql3 = 'SELECT * FROM fish WHERE species=%s'
+        result = db_query.db_query(sql3, [specie])
+        print(result)
+        data = [item[feature] for item in result]
+        print(data)
+        return render_template('underwater_system.html', rank=rank, nums=nums, specie_num=len(species_num),
+                               species=species, data=data, feature=feature, fish=specie,species_num=species_num)
 
 
 @app.route('/data_center')
@@ -107,15 +127,13 @@ def user_register():
         result = db_query.db_query(sql, [user_id])
         print(result)
         if len(result) == 0:
-            sql='insert into user(user_id,password) values(%s,%s)'
-            db_query.db_query(sql, [user_id,password])
-            success='注册成功'
+            sql = 'insert into user(user_id,password) values(%s,%s)'
+            db_query.db_query(sql, [user_id, password])
+            success = '注册成功'
             return render_template('user_login.html', success=success)
         else:
             error = '注册失败'
             return render_template('user_register.html', error=error)
-
-
 
 
 if __name__ == '__main__':
